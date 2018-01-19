ca98e040b6d2e076e8e8462ab7bf73fa0a33f120
liuxl <liuxiaolong@etiaolong.com>
2018-01-19 17:02:45 +0800

    程序入口

--- src/main/java/com/wisty/ts/core/Application.java
+++ src/main/java/com/wisty/ts/core/Application.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/** 
+ * @Description 程序入口
+ * @author 刘小龙
+ * @date 2018年1月19日 下午5:01:56 
+ * @version V1.3.1
+ */ 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
